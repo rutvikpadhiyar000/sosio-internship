8a283fa2e39dbf107fd5afc86fdf39de67979fec
rutvik padhiyar <76583071+rutvikpadhiyar000@users.noreply.github.com>
2021-06-13 01:31:58 +0530

    Removed atmp credentials

--- nseWebscrap/nseWebscrap/settings.py
+++ nseWebscrap/nseWebscrap/settings.py
@@ -133,5 +133,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "rutvikdjangotesting@gmail.com"
-EMAIL_HOST_PASSWORD = "djangotesting"
+EMAIL_HOST_USER = # Add mail address here.
+EMAIL_HOST_PASSWORD = # Add password for mail
